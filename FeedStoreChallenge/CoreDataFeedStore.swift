87e7a3cc2a2df0753341aa0753989a34be86167a
Elkana Orbach <elkorb@gmail.com>
2021-08-19 14:38:42 +0300

    use extracted helper function to translate the models

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -34,11 +34,7 @@ public final class CoreDataFeedStore: FeedStore {
 			do {
 				if let cache = try ManagedCache.find(in: context) {
 					completion(.found(
-						feed: cache.feed
-							.compactMap { ($0 as? ManagedFeedImage) }
-							.map {
-								LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
-							},
+						feed: cache.localFeed,
 						timestamp: cache.timestamp))
 				} else {
 					completion(.empty)
